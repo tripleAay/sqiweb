a72a52899e3b318d8c8b368d74d59d2e8698824f
Adedokun Adeshina <adeshina93@gmail.com>
2024-03-06 05:57:01 -0800

    start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className="mt-5">Hello World</h1>
+     <img src={logo} alt="" />
+    </div>
+  );
+}
+
+export default App;
